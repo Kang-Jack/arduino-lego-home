751d3bedc538c4b6d7c18f31852bdf554a546e9d
Kang <Kang-Jack@users.noreply.github.com>
2018-01-25 21:59:26 +0800

    partially work

--- MotionSensor.ino
+++ MotionSensor.ino
@@ -1,10 +1,10 @@
-char* ssid = "Wi-Fi Example"; //Wi-Fi AP Name
-char* password = "Sample"; //Wi-Fi Password
-char* mqtt_server = "1.1.1.1"; //MQTT Server IP
-char* mqtt_name = "Secondary Bedroom Motion Sensor"; //MQTT device name
-char* mqtt_topic = "SecondaryBedroomMotionSensor"; //MQTT topic for communication
+char* ssid = "xxx"; //Wi-Fi AP Name
+char* password = "xxx"; //Wi-Fi Password
+char* mqtt_server = "192.168.xx.xx"; //MQTT Server IP
+char* mqtt_name = "LegoMotionSensor"; //MQTT device name
+char* mqtt_topic = "lego/motion"; //MQTT topic for communication
 char* mqtt_ending = "/data"; //MQTT subsection for communication
-int pirPin = D0;  //set the GPIO which you will connect the PIR sensor
+int pirPin = 2;  //set the GPIO which you will connect the PIR sensor
 bool lowPower = false; //set to true if you want low power use, slower alerts but more battery
 int delayTime = 2000; //ONLY FOR LOW POWER - how long motion detected should be active
 
@@ -21,36 +21,44 @@ int pir = 1;
 
 char* mqtt_subtopic = "/data"; //MQTT topic for communication
 char* mqtt_maintopic = mqtt_topic;
+//char* mqtt_sub = "home/command/lego/nano";
 
 void setup() {
   strcat(mqtt_maintopic, mqtt_subtopic);
   pinMode(pirPin, INPUT);
-  Serial.begin(74880);
+  Serial.begin(9600);
 }
 
 void loop() {
   if (!lowPower && WiFi.status() != WL_CONNECTED) startWiFi();
-  Serial.println(digitalRead(pirPin));
-
-  if ((digitalRead(pirPin)) == 0) delay(250);
-
+    Serial.println(digitalRead(pirPin));
+  if ((digitalRead(pirPin)) == 0) 
+  { 
+    if (MQTT.connected() && pir == 0)
+    {
+      MQTT.publish(mqtt_topic, "FALSE");
+      //Serial.println("Message Published: FALSE");
+      pir = 1;
+    }
+    delay(250);
+  }
   else {
     if (WiFi.status() != WL_CONNECTED) startWiFi();
     MQTT.loop();
     if (!MQTT.connected()) reconnect();
     MQTT.publish(mqtt_topic, "TRUE");
-    Serial.println("Message Published: TRUE");
+    //Serial.println("Message Published: TRUE");
     while (digitalRead(pirPin) == 1) {
       if (!MQTT.connected()) reconnect();
       MQTT.publish(mqtt_topic, "TRUE");
-      Serial.println("Message Published: TRUE");
-      delay(500);
+      //Serial.println("Message Published: TRUE");
+      delay(200);
     }
     pir = 0;
     if (!MQTT.connected()) reconnect();
     if (lowPower) delay(delayTime);
     MQTT.publish(mqtt_topic, "FALSE");
-    Serial.println("Message Published: FALSE");
+    //Serial.println("Message Published: FALSE");
     MQTT.loop();
     delay(500);
   }
@@ -63,24 +71,24 @@ void loop() {
 }
 
 void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Message arrived [");
+  //Serial.print("Message arrived [");
   Serial.print(topic);
-  Serial.print("] ");
-  for (int i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);
-  }
-  Serial.println();
+  //Serial.print("] ");
+  //for (int i = 0; i < length; i++) {
+  //  Serial.print((char)payload[i]);
+  //}
+  //Serial.println();
 }
 
 void reconnect() {
   while (!MQTT.connected()) {
-    Serial.print("Attempting MQTT connection...");
+    //Serial.print("Attempting MQTT connection...");
     if (MQTT.connect(mqtt_name)) {
-      Serial.println("connected");
+      //Serial.println("connected");
     } else {
-      Serial.print("failed, rc=");
-      Serial.print(MQTT.state());
-      Serial.println(" try again in 5 seconds");
+      //Serial.print("failed, rc=");
+      //Serial.print(MQTT.state());
+      //Serial.println(" try again in 5 seconds");
       for (int i = 0; i < 5000; i++) {
         delay(1);
       }
