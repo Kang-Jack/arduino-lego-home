21d45a5b7df5962fc0053a7ac4ba2277b85ca546
Kang <Kang-Jack@users.noreply.github.com>
2018-01-27 00:01:56 +0800

    esp ok

--- processing/ESPmqtt/ESPmqtt.ino
+++ processing/ESPmqtt/ESPmqtt.ino
@@ -1,8 +1,8 @@
 char* ssid = "xxx"; //Wi-Fi AP Name
 char* password = "xxx"; //Wi-Fi Password
-char* mqtt_server = "192.168.xx.xx"; //MQTT Server IP
-char* mqtt_name = "esp1s"; //MQTT device name
-char* mqtt_topic = "esp1s"; //MQTT topic for communication
+char* mqtt_server = "192.168.xxx.xxx"; //MQTT Server IP
+char* mqtt_name = "ESPSensor"; //MQTT device name
+char* mqtt_topic = "esp/answer" ;//"esp1s"; //MQTT topic for communication
 
 bool lowPower = false; //set to true if you want low power use, slower alerts but more battery
 int delayTime = 2000; //ONLY FOR LOW POWER - how long motion detected should be active
@@ -14,104 +14,38 @@ int delayTime = 2000; //ONLY FOR LOW POWER - how long motion detected should be 
 WiFiClient mainESP;
 PubSubClient MQTT(mainESP);
 
-const int max_length=100;
-char msg[max_length];
+//long lastMsg = 0;
+//char msg[50];
+//int value = 0;
+//int pir = 1;
 
-char* mqtt_subtopic = "/answer/data"; //MQTT topic for communication
+const int max_length=100;
+char msg[100];
+
+char* mqtt_subtopic = "/data"; //"/answer/data"; //MQTT topic for communication
 char* mqtt_maintopic = mqtt_topic;
 char* mqtt_sub = "home/command/esp1s/nano";
 
-void listen()
-{
-    int i = 0;
-    while(Serial.available() > 0) 
-    {
-      char a = Serial.read();
-      if(a == '\0')
-        continue;
-      if(a != '\r' && a != '\n' && (a < 32))
-        continue;
-      if (i<max_length) msg[i] = a ;
-      
-      i++;
-    }
-    if (i==0) return;
-    set_end_mark(i);
-    String msgString((char*)msg);
-//    if (msg[0]== '[')
-//    {
-//      set_config(&msgString);
-//      msg[0]= '\0';
-//      return;
-//    }
-    if (msgString == "received") msg[0]= '\0';
-    else publish_msg((char*)msg);
-}
-//void set_config(char*){
-//  char* str_out="";
-//  if(msg_str.startsWith("[ssid]"))
-//  {
-//     str_out = msg_str.substring(6);
-//     ssid = str_out;
-//  }
-//  else if(msg_str.startsWith("[psw]"))
-//  {
-//    str_out = msg_str.substring(5);
-//    password = str_out;
-//  }
-//  else if(msg_str.startsWith("[m_s]"))
-//  {
-//    str_out = msg_str.substring(5);
-//    mqtt_server = str_out;
-//  }
-//  else if(msg_str.startsWith("[m_n]"))
-//  {
-//    str_out = msg_str.substring(5);
-//     mqtt_name = str_out;
-//  }
-//  else if(msg_str.startsWith("[m_t]"))
-//  {
-//    str_out = msg_str.substring(5);
-//    mqtt_topic = str_out;
-//  }
-//  else if(msg_str.startsWith("[m_sub]"))
-//  {
-//    str_out = msg_str.substring(7);
-//    mqtt_sub = str_out;
-//  }
-//  Serial.print("settings changed");
-//  Serial.print(mqtt_sub);
-//}
-void publish_msg(char* msg_str)
-{
-    if ( WiFi.status() != WL_CONNECTED) startWiFi();
-    if (!MQTT.connected()) reconnect();
-    MQTT.publish(mqtt_topic, msg_str);
-    delay(200);
-}
-
-void talk()
-{
-    if(msg[0]!='\0'){ 
-      Serial.print((char*)msg);
-      //msg[0] = '\0';
-    }
- }
 
 void setup() {
   strcat(mqtt_maintopic, mqtt_subtopic);
-  msg[0] = '\0';
   Serial.begin(9600);
+ // msg[0] = '\0';
 }
 
 void loop() {
   if (!lowPower && WiFi.status() != WL_CONNECTED) startWiFi();
-  talk();
+  //delay(200);
+  //if (!MQTT.connected()) reconnect();
+  //MQTT.publish(mqtt_topic, "TRUE");
+  //MQTT.loop();
+  talk_master();
 
-  listen();
-
+  listen_master();
+  if (!MQTT.connected()) reconnect();
   MQTT.loop();
   delay(200);
+
   if (lowPower) {
     WiFi.disconnect();
     WiFi.mode(WIFI_OFF);
@@ -120,13 +54,17 @@ void loop() {
   }
 }
 
+
+
+
 void callback(char* topic, byte* payload, unsigned int length) {
-  //Serial.print(topic);
+  Serial.print(topic);
   int i=0;
   for(i=0; i<length; i++) {
       if (i<max_length) msg[i] = (char)payload[i];
   }
   set_end_mark(i);
+  Serial.println();
 }
 
 void set_end_mark(int i){
@@ -140,14 +78,16 @@ void reconnect() {
     Serial.print("Attempting MQTT connection...");
     if (MQTT.connect(mqtt_name)) {
       Serial.println("connected MQTT");
+      Serial.println(mqtt_maintopic);
+      Serial.println(mqtt_sub);
       // Once connected, publish an announcement...
       MQTT.publish(mqtt_maintopic,"hello-ESP1s");
       // ... and resubscribe
       MQTT.subscribe(mqtt_sub);
     } else {
-      //Serial.print("failed, rc=");
-      Serial.print(MQTT.state());
-      //Serial.println(" try again in 5 seconds");
+      Serial.print("failed, rc=");
+      Serial.println(MQTT.state());
+      Serial.println(" try again in 5 seconds");
       for (int i = 0; i < 5000; i++) {
         delay(1);
       }
@@ -169,4 +109,51 @@ void startWiFi() {
   Serial.println(WiFi.localIP());
   MQTT.setServer(mqtt_server, 1883);
   MQTT.setCallback(callback);
+}
+
+
+void listen_master()
+{
+    int i = 0;
+    while(Serial.available() > 0) 
+    {
+      char a = Serial.read();
+      if(a == '\0')
+        continue;
+      if(a != '\r' && a != '\n' && (a < 32))
+        continue;
+      if (i<max_length) msg[i] = a ;
+      
+      i++;
+    }
+    if (i==0) return;
+    set_end_mark(i);
+    String msgString((char*)msg);
+    if (msgString == "received") 
+    {
+      msg[0]= '\0';
+      Serial.println("ok received");
+    }
+    else {
+      publish_msg((char*)msg);
+      msg[0]= '\0';
+      Serial.println();
+      Serial.println(msgString);
+    }
+}
+
+void publish_msg(char* msg_str)
+{
+    if ( WiFi.status() != WL_CONNECTED) startWiFi();
+    if (!MQTT.connected()) reconnect();
+    MQTT.publish(mqtt_topic, msg_str);
+    delay(200);
+}
+
+void talk_master()
+{
+    if(msg[0]!='\0'){ 
+      Serial.print((char*)msg);
+      //msg[0] = '\0';
+    }
 }
