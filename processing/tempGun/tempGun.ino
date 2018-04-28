bec764f6c5c3494995da290dfcdda88b7c002255
Kang <Kang-Jack@users.noreply.github.com>
2018-04-28 22:26:05 +0800

    temperature and ole ready

--- processing/tempGun/tempGun.ino
+++ processing/tempGun/tempGun.ino
@@ -1,73 +1,108 @@
 
 #include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
-#define OLED_RESET 4
-Adafruit_SSD1306 display(OLED_RESET);
+#include "SSD1306.h"
+
+const int pinSDA = 4;
+const int pinSCL = 5;
+SSD1306  display(0x3c, pinSDA, pinSCL);
 
 #include <Adafruit_MLX90614.h>
 Adafruit_MLX90614 mlx = Adafruit_MLX90614();
 
 const int buttonPin = 11;    // the number of the pushbutton pin
 
-void set_oled(int x,int y,int font_size) {
-  display.setTextSize(font_size);
-  display.setTextColor(WHITE);
-  display.setCursor(x,y);
-  display.clearDisplay();
+void init_oled() {
+  display.init();
+  display.flipScreenVertically();
+  display.clear();
+  display.setColor(WHITE);
+  display.setTextAlignment(TEXT_ALIGN_LEFT);
+  display.setFont(ArialMT_Plain_24);
+  display.drawString(0, 0, "Init OLED");
 }
-void new_line_oled(char* mesg){
-    display.clearDisplay();
-    display.println(mesg);
-    display.display();
-    delay(100);
+void set_s_text(){
+    display.setTextAlignment(TEXT_ALIGN_LEFT);
+    display.setFont(ArialMT_Plain_10);;
 }
-void append_line_oled(char* mesg){
-    display.println(mesg);
-    delay(100);
+void set_m_text(){
+    display.setTextAlignment(TEXT_ALIGN_LEFT);
+    display.setFont(ArialMT_Plain_16);;
+}
+void set_b_text(){
+    display.setTextAlignment(TEXT_ALIGN_LEFT);
+    display.setFont(ArialMT_Plain_24);
 }
 
-void display_temp(){
-    set_oled(0,0,2);
-    display.println("Object = ");
-    display.println(mlx.readObjectTempC());
-    display.print(" *C");
+void new_screen_oled(int x,int y,char* mesg){
+  display.clear();
+  display.drawString(x, y, mesg);
+  display.display();
+  delay(1000);
+}
+
+
+void display_F_temp(){
+    display.clear();
+    display.setLogBuffer(5, 30);
+    set_s_text();
+    display.println("Object (*F) = ");
     display.println( mlx.readObjectTempF());
-    display.print(" *F");
-    display.println("====================");
-    display.println("Ambient = ");
-    display.println( mlx.readAmbientTempC());
-    display.print(" *C");
+    Serial.println("====================");
+    display.println("Ambient (*F) = ");
     display.println( mlx.readAmbientTempF());
-    display.print(" *F");
-    delay(1000);
-    display.clearDisplay();
+    display.drawLogBuffer(0, 0);
+    display.display();
+    delay(2000);
+
+}
+
+void display_object_temp(){
+    display.clear();
+    set_m_text();
+    display.drawString(0, 0,"Object (*C) = ");
+    set_b_text();
+    display.drawString(0,22,String(mlx.readObjectTempC()));
+    display.display();
+    delay(2000);
+}
+
+void display_ambient_temp(){
+    display.clear();  
+    set_m_text();
+    display.drawString(0,0,"Ambient (*C) = ");
+    set_b_text();
+    display.drawString(0,22, String(mlx.readAmbientTempC()));
+    display.display();
+    delay(2000);
 }
 
 void setup(void){
-
+  //Wire.pins(int sda, int scl);
+  Wire.pins(pinSDA, pinSCL);
   Serial.begin(115200);
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  //0x3C initialize with the I2C addr 0x3D (for the 128x64)
-  display.display();
-  delay(2000);
-  set_oled(0,0,1);
-
-   if (!mlx.begin()) 
+  init_oled();
+  Serial.println("init");
+ if (!mlx.begin()) 
   {
-     new_line_oled("Could not find MLX90614 sensor at 0x5A");
+     new_screen_oled(0,0,"Could not find MLX90614 sensor");
     while (1) {}
   }
 
-  pinMode(buttonPin, INPUT);
-    
-  new_line_oled("Starting ESP12F2");
+  //pinMode(buttonPin, INPUT);
+  Serial.println("start");
+  new_screen_oled(0,0,"Starting ESP12F2");
 
 }
 
   void loop(void){
-    int reading = digitalRead(buttonPin);
-    if(reading==HIGH)
-    {
-        display_temp();  
-    }
+    //int reading = digitalRead(buttonPin);
+    //if(reading==HIGH)
+    //{
+       //new_screen_oled(0,0," button pushed");
+        display_object_temp();
+        display_ambient_temp(); 
+        display_F_temp();
+        Serial.println("===================="); 
+   // }
+    //new_screen_oled(0,0,"No button pushed");
 }
