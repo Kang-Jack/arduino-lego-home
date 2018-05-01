5987e5fe17331081f4b7ae9428239744f88e3bec
Kang <Kang-Jack@users.noreply.github.com>
2018-05-01 09:48:56 +0800

    support bmp280 and MLX90614

--- processing/tempGun/tempGun.ino
+++ processing/tempGun/tempGun.ino
@@ -6,10 +6,17 @@ const int pinSDA = 4;
 const int pinSCL = 5;
 SSD1306  display(0x3c, pinSDA, pinSCL);
 
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BMP280.h>
+
+#define SEALEVELPRESSURE_HPA (1013.25)
+Adafruit_BMP280 bmp; // I2C
+
 #include <Adafruit_MLX90614.h>
 Adafruit_MLX90614 mlx = Adafruit_MLX90614();
 
 const int buttonPin = 14;    // the number of the pushbutton pin
+
 
 void init_oled() {
   display.init();
@@ -45,10 +52,10 @@ void display_F_temp(){
     display.clear();
     display.setLogBuffer(5, 30);
     set_s_text();
-    display.println("Object (*F) = ");
+    display.println("Object (*F)");
     display.println( mlx.readObjectTempF());
     Serial.println("====================");
-    display.println("Ambient (*F) = ");
+    display.println("Ambient (*F)");
     display.println( mlx.readAmbientTempF());
     display.drawLogBuffer(0, 0);
     display.display();
@@ -59,7 +66,7 @@ void display_F_temp(){
 void display_object_temp(){
     display.clear();
     set_m_text();
-    display.drawString(0, 0,"Object (*C) = ");
+    display.drawString(0, 0,"Object (*C)");
     set_b_text();
     display.drawString(0,22,String(mlx.readObjectTempC()));
     display.display();
@@ -69,9 +76,41 @@ void display_object_temp(){
 void display_ambient_temp(){
     display.clear();  
     set_m_text();
-    display.drawString(0,0,"Ambient (*C) = ");
+    display.drawString(0,0,"Ambient (*C)");
     set_b_text();
     display.drawString(0,22, String(mlx.readAmbientTempC()));
+    display.display();
+    delay(2000);
+}
+
+void display_bme_temp() {
+
+    display.clear();  
+    set_m_text();
+    display.drawString(0,0,"Temperature (*C)");
+    set_b_text();
+    display.drawString(0,22, String(bmp.readTemperature()));
+    display.display();
+    delay(2000);
+}
+
+void display_bme_Pressure() {
+
+    display.clear();  
+    set_m_text();
+    display.drawString(0,0,"Pressure (hPa)");
+    set_b_text();
+    display.drawString(0,22, String(bmp.readPressure() / 100.0F));
+    display.display();
+    delay(2000);
+}
+
+void display_bme_Altitude() {
+    display.clear();  
+    set_m_text();
+    display.drawString(0,0,"Calc.Altitude (m)");
+    set_b_text();
+    display.drawString(0,22, String(bmp.readAltitude(SEALEVELPRESSURE_HPA)));
     display.display();
     delay(2000);
 }
@@ -82,31 +121,48 @@ void setup(void){
   Serial.begin(115200);
   init_oled();
   Serial.println("init");
+  
  if (!mlx.begin()) 
   {
-     new_screen_oled(0,0,"Could not find MLX90614 sensor");
+    set_s_text();
+    new_screen_oled(0,0,"Not find MLX90614");
     while (1) {}
+  }
+
+  bool status;
+  
+  // default settings
+  // (you can also pass in a Wire library object like &Wire2)
+  status = bmp.begin();  
+  if (!status) {
+      set_s_text();
+      new_screen_oled(0,0,"Not find BMP280");
+      while (1);
   }
 
   pinMode(buttonPin, INPUT);
   Serial.println("start");
-  new_screen_oled(0,0,"Starting ESP12F2");
+  new_screen_oled(0,0,"Starting ESP12S");
 
 }
 
-  void loop(void){
-    int reading = digitalRead(buttonPin);
-    Serial.println(reading); 
-    if(reading==LOW)
-    {
-       //new_screen_oled(0,0," button pushed");
-        display_object_temp();
-        display_ambient_temp(); 
-        display_F_temp();
-        Serial.println("===================="); 
-    }
-    else
-    {
-      new_screen_oled(0,0,"No button pushed");
-    }
+void loop(void){
+  int reading = digitalRead(buttonPin);
+  if(reading==LOW)
+  {
+      set_m_text();
+      new_screen_oled(0,0," button pushed");
+      display_object_temp();
+      display_ambient_temp(); 
+      display_F_temp();
+      Serial.println("===================="); 
+  }
+  else
+  {
+    set_m_text();
+    new_screen_oled(0,0," no button pushed");
+    display_bme_temp();
+    display_bme_Pressure();
+    display_bme_Altitude();
+  }
 }
